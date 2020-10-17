3d20095dfdfa678e4dd47f49c5bbbbe5be0a3233
Mario <nogueira.mario@gmail.com>
2020-10-17 13:04:02 -0300

    novo projeto

---
+++ exec_cap10_fix/Entities/TaxPayer.cs
@@ -0,0 +1,15 @@
+﻿namespace exec_cap10_fix.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        protected TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+        public abstract double Tax();
+    }
+}
---
+++ exec_cap10_fix/Program.cs
@@ -0,0 +1,49 @@
+﻿using exec_cap10_fix.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace exec_cap10_fix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+            List<TaxPayer> list = new List<TaxPayer>(n);
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data:");
+                Console.Write("Individual or company (i/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double anualincome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 'c')
+                {
+                    Console.Write("Numer of employees: ");
+                    int numberEmployees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(name, anualincome, numberEmployees));
+                }
+                else
+                {
+                    Console.Write("Health expenditures: ");
+                    double healhExpenditures = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Individual(name, anualincome, healhExpenditures));
+                }
+            }
+            double sum = 0;
+            Console.WriteLine();
+            Console.WriteLine("TAXES PAID:");
+            foreach (TaxPayer tax in list)
+            {
+                Console.WriteLine(tax.Name + ": $ " + tax.Tax().ToString("F2", CultureInfo.InvariantCulture));
+                sum += tax.Tax();
+            }
+            Console.WriteLine();
+            Console.WriteLine("TOTAL TAXES: $ " + sum.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
